caec5097d1a6b4548fef209125c894227b1cf22e
Termuxschool <73558017+Termuxschool@users.noreply.github.com>
2021-01-19 21:28:26 -0300

    Add files via upload

---
+++ src/bruxinhomenu.js
@@ -0,0 +1,12 @@
+const txslmenu = (prefix, pushname) => {
+    return `◪ *Comandos do TXSL BOT*
+    │
+    ├─ ➤ ${prefix}setprefix
+    ├─ ➤ ${prefix}block
+    ├─ ➤ ${prefix}bc
+    ├─ ➤ ${prefix}bcgc
+    └─ ➤ ${prefix}clearall`
+
+}
+
+exports.txslmenu = txsl
